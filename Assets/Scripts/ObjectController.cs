778bd96aea27bde3bfafb14a2b03b211f7b42642
kchun0513 <kchun0513@hanyang.ac.kr>
2025-03-29 13:07:30 +0900

    SCRUM-45 <점수 오브젝트 추가 및 오브젝트 속도 랜덤 설정>

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ObjectController : MonoBehaviour
 {
     public GameObject ObjGenerator;
-    public int speed;
+    public int minSpeed;
+    public int maxSpeed;
+    private int speed;
     // Start is called before the first frame update
     void Start()
     {
+        minSpeed = -minSpeed;
+        maxSpeed = -maxSpeed;
+        speed = Random.Range(maxSpeed, minSpeed+1);
         transform.position = ObjGenerator.transform.position;
-        print(transform.position);
+        print(speed);
     }
 
     // Update is called once per frame
